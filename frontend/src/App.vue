4338d1fd80568351113cb2e92abc1b7331e5df1f
vsevolodgarkusa <vsevolod2010@list.ru>
2021-04-03 22:40:17 +0300

    new front

---
+++ frontend/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <theme-tile v-for="theme in themes" :theme="theme"></theme-tile>
+      <topic-tile v-for="topic in topics" :topic="topic"></topic-tile>
+    </div>
+  </div>
+</template>
+
+<script>
+import ThemeTile from "@/components/ThemeTile";
+import TopicTile from '@/components/TopicTile';
+export default {
+  name: 'App',
+  components: {
+    ThemeTile,
+    TopicTile
+  },
+  data: function() {
+    return {
+      themes: [
+        {"id": 1, "title": "Internet", "left": 500, "top": 100},
+        {"id": 2, "title": "Basic Frontend Knowledge", "left": 520, "top": 200},
+        {"id": 3, "title": "OS and Genereal Knowledge", "left": 550, "top": 300}
+      ],
+      topics: [
+        {"id": 1, "title": "How does the internet work?", "theme": 1, "left": 200, "top": 50},
+        {"id": 2, "title": "What is HTTP?", "theme": 1, "left": 200, "top": 120},
+        {"id": 3, "title": "Browsers and how they work?", "theme": 1, "left": 200, "top": 170},
+        {"id": 4, "title": "DNS and how it works?", "theme": 1, "left": 800, "top": 50},
+        {"id": 5, "title": "What is Domain Name?", "theme": 1, "left": 800, "top": 120},
+        {"id": 6, "title": "What is hosting?", "theme": 1, "left": 800, "top": 170},
+        {"id": 7, "title": "HTML", "theme": 2, "left": 200, "top": 230},
+        {"id": 8, "title": "CSS", "theme": 2, "left": 800, "top": 230},
+        {"id": 9, "title": "JavaScript", "theme": 2, "left": 800, "top": 280},
+        {"id": 10, "title": "Terminal Usage", "theme": 3, "left": 200, "top": 500},
+        {"id": 11, "title": "How OSs work in General", "theme": 3, "left": 200, "top": 550},
+        {"id": 12, "title": "Process Management", "theme": 3, "left": 200, "top": 600},
+        {"id": 13, "title": "Threads and Concurrency", "theme": 3, "left": 200, "top": 650},
+        {"id": 14, "title": "Basic Terminal Commands", "theme": 3, "left": 200, "top": 700},
+        {"id": 15, "title": "Memory Management", "theme": 3, "left": 200, "top": 750},
+        {"id": 16, "title": "Interprocess Communication", "theme": 3, "left": 200, "top": 800},
+        {"id": 17, "title": "I/O Management", "theme": 3, "left": 200, "top": 850},
+        {"id": 18, "title": "POSIX Basics", "theme": 3, "left": 200, "top": 900},
+        {"id": 19, "title": "Basic Networking Concepts", "theme": 3, "left": 200, "top": 950}
+      ]
+    }
+},
+}
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 2000px;
+  display: inline-block;
+}
+</style>
---
+++ frontend/src/components/TopicTile.vue
@@ -0,0 +1,59 @@
+<template>
+<vue-resizable
+    :id="'main-topic'+topic.id"
+    :left="topic.left"
+    :top="topic.top"
+    fit-parent
+    drag-selector=".content"
+    class="topic-tile"
+    active=""
+>
+    <div class="content" :id="'topic'+topic.id">
+        <input type="checkbox" class="checkbox">
+        {{topic.title}}
+    </div>
+</vue-resizable>
+</template>
+
+<script>
+import VueResizable from 'vue-resizable'
+import Checkbox from 'vue-material-checkbox'
+
+export default {
+    name: "TopicTile",
+    props: ['topic'],
+    components: {
+        VueResizable,
+        Checkbox
+    },
+    mounted() {
+        this.$children[0].w = parseInt(this.$el.style.width)
+        this.$children[0].h = parseInt(this.$el.style.height)
+    }
+}
+</script>
+
+<style scoped>
+.content {
+    width: auto;
+    height: auto;
+    min-width: 100px;
+    max-width: 200px;
+    border-radius: 2px;
+    border: 1px solid black;
+    text-align: center;
+    padding: 15px;
+    background-color: rgb(222, 135, 135);
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 15px;
+}
+.topic-tile {
+    position: absolute;
+}
+.checkbox {
+    position: absolute;
+    left: -5px;
+    top: -5px;
+    border-radius: 50%;
+}
+</style>
